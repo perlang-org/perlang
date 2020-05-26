296641d1ef481c40b44b3c5c2ac1978a0a1ffdff
Per Lundberg <perlun@gmail.com>
2020-05-26 22:41:37 +0300

    (tests) #46 Add 'return' tests (#51)

---
+++ Perlang.Tests/Return.cs
@@ -0,0 +1,126 @@
+using System.Linq;
+using Xunit;
+using static Perlang.Tests.EvalHelper;
+
+namespace Perlang.Tests
+{
+    public class Return
+    {
+        // Tests based on Lox test suite: https://github.com/munificent/craftinginterpreters/tree/master/test/return
+
+        [Fact]
+        void after_else()
+        {
+            string source = @"
+                fun f() {
+                  if (false) ""no""; else return ""ok"";
+                }
+
+                print f();
+            ";
+
+            string output = EvalReturningOutput(source).SingleOrDefault();
+
+            Assert.Equal("ok", output);
+        }
+
+        [Fact]
+        void after_if()
+        {
+            string source = @"
+                fun f() {
+                  if (true) return ""ok"";
+                }
+
+                print f();
+            ";
+
+            string output = EvalReturningOutput(source).SingleOrDefault();
+
+            Assert.Equal("ok", output);
+        }
+
+        [Fact]
+        void after_while()
+        {
+            string source = @"
+                fun f() {
+                  while (true) return ""ok"";
+                }
+
+                print f();
+            ";
+
+            string output = EvalReturningOutput(source).SingleOrDefault();
+
+            Assert.Equal("ok", output);
+        }
+
+        [Fact]
+        void at_top_level()
+        {
+            string source = @"
+                return ""wat"";
+            ";
+
+            var result = EvalWithResolveErrorCatch(source);
+            var exception = result.ResolveErrors.First();
+
+            Assert.Single(result.ResolveErrors);
+            Assert.Matches("Cannot return from top-level code.", exception.Message);
+        }
+
+        [Fact]
+        void in_function()
+        {
+            string source = @"
+                fun f() {
+                  return ""ok"";
+                  print ""bad"";
+                }
+
+                print f();
+            ";
+
+            string output = EvalReturningOutput(source).SingleOrDefault();
+
+            Assert.Equal("ok", output);
+        }
+
+        [Fact(Skip = "Disabled until we have implemented support for classes")]
+        void in_method()
+        {
+            string source = @"
+                class Foo {
+                  method() {
+                    return ""ok""
+                    print ""bad"";
+                  }
+                }
+
+                print Foo().method();
+            ";
+
+            string output = EvalReturningOutput(source).SingleOrDefault();
+
+            Assert.Equal("ok", output);
+        }
+
+        [Fact]
+        void return_nil_if_no_value()
+        {
+            string source = @"
+                fun f() {
+                  return;
+                  print ""bad"";
+                }
+
+                print f();
+            ";
+
+            string output = EvalReturningOutput(source).SingleOrDefault();
+
+            Assert.Equal("nil", output);
+        }
+    }
+}
