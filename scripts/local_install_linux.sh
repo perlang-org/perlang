ea4f64925b3263cfe1c8bdaffe42b2e96244add9
Per Lundberg <perlun@gmail.com>
2020-06-14 08:29:02 +0300

    (docs) Update README.md, minor installer tweaks (#59)

--- scripts/local_install_linux.sh
+++ scripts/local_install_linux.sh
@@ -2,14 +2,14 @@
 
 #
 # Builds and installs a local version of the Perlang toolchain, in
-# ~/.perlang/bin
+# ~/.perlang/nightly/bin
 #
 
 set -e
 
-mkdir -p $HOME/.perlang/bin
+mkdir -p $HOME/.perlang/nightly/bin
 
 pushd Perlang.ConsoleApp
 dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishReadyToRun=true
-cp -r bin/Release/netcoreapp3.1/linux-x64/publish/* $HOME/.perlang/bin
+cp -r bin/Release/netcoreapp3.1/linux-x64/publish/* $HOME/.perlang/nightly/bin
 popd
