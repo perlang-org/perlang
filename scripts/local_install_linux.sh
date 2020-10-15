64604cd2eb0684e17e846aad9b6822b7134eb3f8
Per Lundberg <perlun@gmail.com>
2020-10-15 22:26:30 +0300

    (scripts) Fix local_install_linux to work again
    It has been broken as of #89. The fix is similar in nature to #93.

--- scripts/local_install_linux.sh
+++ scripts/local_install_linux.sh
@@ -9,7 +9,5 @@ set -e
 
 mkdir -p $HOME/.perlang/nightly/bin
 
-pushd Perlang.ConsoleApp
-dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishReadyToRun=true
-cp -r bin/Release/netcoreapp3.1/linux-x64/publish/* $HOME/.perlang/nightly/bin
-popd
+dotnet publish Perlang.ConsoleApp/Perlang.ConsoleApp.csproj -c Release -r linux-x64 --self-contained true /p:PublishReadyToRun=true /p:SolutionDir=$(pwd)/
+cp -r Perlang.ConsoleApp/bin/Release/netcoreapp3.1/linux-x64/publish/* $HOME/.perlang/nightly/bin
