370b1d6f19d0b73980cd2be2714b0df5572e093b
Per Lundberg <perlun@gmail.com>
2022-02-25 22:33:24 +0200

    (scripts) Add support for custom version

--- scripts/update_common_constants.rb
+++ scripts/update_common_constants.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 
+# If a version is provided on the command line, it will take precedence over
+# `git describe` output. This is used to produce beta & stable releases.
+CUSTOM_VERSION = ARGV.pop
+
 # Need to take the dev/ part out of e.g. dev/0.1.0, since assembly versions must
 # adhere to the following format: major[.minor[.build[.revision]]]
-GIT_TAG_VERSION = `git describe --tags --abbrev=0 | sed s%^dev/%%`.rstrip
+GIT_TAG_VERSION = CUSTOM_VERSION || `git describe --tags --abbrev=0 | sed s%^dev/%%`.rstrip
 
 # The input to these sed operations is something like `dev/0.1.0-224-g09f4704`.
 # The output is expected to produce a SemVer-compliant version number. For
 # snapshots, it will be something like 0.1.0-dev.224
-GIT_DESCRIBE_VERSION = `git describe --tags | sed s%^dev/%% | sed s/-g.*$// | sed -E "s/-([0-9]*)$/-dev.\\1/"`.rstrip
+GIT_DESCRIBE_VERSION = CUSTOM_VERSION || `git describe --tags | sed s%^dev/%% | sed s/-g.*$// | sed -E "s/-([0-9]*)$/-dev.\\1/"`.rstrip
 
 GIT_COMMIT = `git describe --always`.rstrip
 
@@ -29,4 +33,4 @@ EOF
 path = File.join('src', 'Perlang.Common', 'CommonConstants.Generated.cs')
 File.write(path, common_constants)
 
-puts("#{path} updated")
+puts("#{path} updated with version #{GIT_DESCRIBE_VERSION}")
