633b584707abadb8ddf76f6a8522fe5168e98adf
Per Lundberg <perlun@gmail.com>
2022-04-27 21:33:55 +0300

    (tests) Add full type coverage for << and >> operators (#325)
    See #231 for the bigger picture.

--- src/Perlang.Tests.Integration/Operator/Binary/ShiftRightTests.cs
+++ src/Perlang.Tests.Integration/Operator/Binary/ShiftRightTests.cs
@@ -22,7 +22,7 @@ public class ShiftRightTests
     }
 
     [Theory]
-    [MemberData(nameof(BinaryOperatorData.GreaterGreater_type), MemberType = typeof(BinaryOperatorData))]
+    [MemberData(nameof(BinaryOperatorData.ShiftRight_type), MemberType = typeof(BinaryOperatorData))]
     public void with_supported_types_returns_expected_type(string i, string j, string expectedResult)
     {
         string source = $@"
