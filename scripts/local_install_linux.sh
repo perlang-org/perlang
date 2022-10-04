23622b070b01f91aa23f9c49824d3ad5c410c001
Per Lundberg <perlun@gmail.com>
2022-10-04 20:33:03 +0300

    (many) Upgrade to .NET 7 RC1

--- scripts/local_install_linux.sh
+++ scripts/local_install_linux.sh
@@ -10,4 +10,4 @@ set -e
 mkdir -p $HOME/.perlang/nightly/bin
 
 dotnet publish src/Perlang.ConsoleApp/Perlang.ConsoleApp.csproj -c Release -r linux-x64 --self-contained true /p:PublishReadyToRun=true /p:SolutionDir=$(pwd)/
-cp -r src/Perlang.ConsoleApp/bin/Release/net6.0/linux-x64/publish/* $HOME/.perlang/nightly/bin
+cp -r src/Perlang.ConsoleApp/bin/Release/net7.0/linux-x64/publish/* $HOME/.perlang/nightly/bin
