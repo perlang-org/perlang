c6472edcc228de05a0aa15c9fafaafc89eb68d2e
Per Lundberg <perlun@gmail.com>
2024-05-29 22:53:54 +0300

    (compiler) Add `--idempotent` flag (#486)
    This is useful when committing the generated C++ files to version
    control, which we will soon start doing ourselves. :)

--- src/Perlang.Common/Compiler/CompilerFlags.cs
+++ src/Perlang.Common/Compiler/CompilerFlags.cs
@@ -18,8 +18,14 @@ public enum CompilerFlags
     CacheDisabled = 1,
 
     /// <summary>
-    /// This flag which makes the compiler remove the `main` method if it is empty. This
-    /// can be useful e.g. when the `main` method is implemented in C++.
+    /// This flag which makes the compiler remove the `main` method if it is empty. This can be useful e.g. when the
+    /// `main` method is implemented in C++.
     /// </summary>
-    RemoveEmptyMainMethod = 2
+    RemoveEmptyMainMethod = 2,
+
+    /// <summary>
+    /// This flag makes the compiler avoid writing timestamps and the Perlang compiler version to generated C++ files.
+    /// This is useful when these files are committed to version control, to avoid redundant diffs.
+    /// </summary>
+    Idempotent = 4
 }
