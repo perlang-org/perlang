eb8cd8c131bbfbc568380a2b16c56c04440e58a9
Per Lundberg <perlun@gmail.com>
2024-06-01 22:12:58 +0300

    (many) Add explicitly typed `int[]` arrays

--- src/Perlang.Tests.Integration/Arrays/IntArrayTests.cs
+++ src/Perlang.Tests.Integration/Arrays/IntArrayTests.cs
@@ -6,7 +6,7 @@ namespace Perlang.Tests.Integration.Arrays;
 
 public class IntArrayTests
 {
-    [Fact(Skip = "Explicitly typed arrays are not yet supported")]
+    [Fact]
     public void explicitly_typed_int_array_can_be_indexed()
     {
         string source = """
@@ -48,5 +48,44 @@ public class IntArrayTests
             );
     }
 
-    // TODO: Add test for indexing before and after array size, ensuring that we get the expected exceptions
+    [Fact]
+    public void indexing_int_array_with_negative_index_produces_expected_error()
+    {
+        string source = """
+                var a = [1, 2, 3];
+
+                print a[-1];
+                """;
+
+        var result = EvalWithRuntimeErrorCatch(source);
+
+        result.Errors.Should()
+            .ContainSingle()
+            .Which
+            .Message.Should().Contain("exited with exit code 134");
+
+        result.OutputAsString.Should()
+            .Contain("index out of range (18446744073709551615 > 2)");
+    }
+
+    [Fact]
+    public void indexing_int_array_outside_of_boundaries_produces_expected_error()
+    {
+        string source = """
+                var a = [1, 2, 3];
+
+                // a[2] is the last element of the array
+                print a[3];
+                """;
+
+        var result = EvalWithRuntimeErrorCatch(source);
+
+        result.Errors.Should()
+            .ContainSingle()
+            .Which
+            .Message.Should().Contain("exited with exit code 134");
+
+        result.OutputAsString.Should()
+            .Contain("index out of range (3 > 2)");
+    }
 }
