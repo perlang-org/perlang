c45bd2e079d4516956bc6d78ada32a8949d29e80
Per Lundberg <perlun@gmail.com>
2024-10-14 18:15:20 +0000

    (tests) Minor `EqualTests` cleanups
    - Fix formatting (use single level of indentation for raw strings)
    - Avoid `[SkippableTheory]` for tests where we shouldn't be skipping any
      test cases.
    
    https://gitlab.perlang.org/perlang/perlang/-/merge_requests/537

--- src/Perlang.Tests.Integration/Operator/Binary/EqualTests.cs
+++ src/Perlang.Tests.Integration/Operator/Binary/EqualTests.cs
@@ -6,16 +6,16 @@ namespace Perlang.Tests.Integration.Operator.Binary;
 
 public class EqualTests
 {
-    [SkippableTheory]
+    [Theory]
     [MemberData(nameof(BinaryOperatorData.Equal), MemberType = typeof(BinaryOperatorData))]
     void performs_equality_comparison(string i, string j, string expectedResult)
     {
-        string source = $@"
-                var i1 = {i};
-                var i2 = {j};
+        string source = $"""
+            var i1 = {i};
+            var i2 = {j};
 
-                print i1 == i2;
-            ";
+            print i1 == i2;
+            """;
 
         string result = EvalReturningOutputString(source)
             .ToLower();
@@ -30,12 +30,12 @@ public class EqualTests
     [InlineData("foo", "foo", "true")]
     void strings_can_be_compared_for_equality(string i, string j, string expectedResult)
     {
-        string source = $@"
-                var i1 = ""{i}"";
-                var i2 = ""{j}"";
+        string source = $"""
+            var i1 = "{i}";
+            var i2 = "{j}";
 
-                print i1 == i2;
-            ";
+            print i1 == i2;
+            """;
 
         // "True" in interpreted mode vs "true" in compiled mode
         string result = EvalReturningOutputString(source)
